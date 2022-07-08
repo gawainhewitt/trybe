a0fb38e1c39639d28ab188b8cf14f02145b97f2a
Luke Storey <lukemstorey@gmail.com>
2022-07-08 20:20:48 +0100

    Add tab icon styling

--- TrybeFrontend/App.js
+++ TrybeFrontend/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 import Goals from "./src/screens/Goals";
+import Support from "./src/screens/Support";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider } from 'react-redux';
-import rootReducer from './store/rootReducer';
-import { configureStore } from '@reduxjs/toolkit';
-
+import { Provider } from "react-redux";
+import rootReducer from "./store/rootReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { Feather } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
 });
 
 function App() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: { paddingBottom: 2 },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Goals") {
+            iconName = "target";
+          } else if (route.name === "Support") {
+            iconName = "eye";
+          }
+          return <Feather name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Goals" component={Goals} />
+      <Tab.Screen name="Support" component={Support} />
     </Tab.Navigator>
   );
 }
---
+++ TrybeFrontend/src/screens/Support.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, SafeAreaView } from "react-native";
+
+function Support() {
+  return (
+    <SafeAreaView>
+      <Text>Support Tab</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Support;

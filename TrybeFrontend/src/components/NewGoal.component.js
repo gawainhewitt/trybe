92203bc2646a0eaecd5c797f860922029ab5e099
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-07-11 13:43:54 +0100

    error message when empty goal attempted

--- TrybeFrontend/src/components/NewGoal.component.js
+++ TrybeFrontend/src/components/NewGoal.component.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Keyboard, View } from "react-native";
+import { Keyboard, View, Alert } from "react-native";
 import { Button, TextInput } from "react-native-paper";
 import styles from "./NewGoal.component.style";
 import { uploadGoal } from "../../store/goals/goals.actions";
@@ -25,9 +25,21 @@ function NewGoal() {
         mode="contained"
         icon="plus-circle"
         onPress={async () => {
-          dispatch(uploadGoal({ token: user.id, text: text }));
+          if(text != "") {
+            dispatch(uploadGoal({ token: user.id, text: text }));
           onChangeText("");
           Keyboard.dismiss();
+          }else{
+            const createTwoButtonAlert = (title, msg) =>
+              Alert.alert(
+                title,
+                msg,
+                [
+                  { text: "OK", onPress: () => console.log("OK Pressed") }
+                ]
+              );
+            createTwoButtonAlert("Invalid Input!", "No goal specified");
+          }
         }}
       >
         Create Goal

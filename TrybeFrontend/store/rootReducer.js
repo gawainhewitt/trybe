9734d9e50b2b58b272f90a89e2a59ce22e5868c8
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-07-12 15:55:40 +0100

    added supporters Goals to reducer

--- TrybeFrontend/store/rootReducer.js
+++ TrybeFrontend/store/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers  } from "redux";
 import goalReducer from "./goals/goals.reducers";
+import supporterGoalsReducer from "./goals/supporterGoals.reducers";
+
 export default combineReducers({
-  goals: goalReducer
+  goals: goalReducer,
+  supporterGoals: supporterGoalsReducer
 });
